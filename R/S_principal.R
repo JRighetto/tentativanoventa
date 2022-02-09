0fdf4519f5c6714f6868e3b4d754f1b133e8835d
JRighetto <jurighcorrea@gmail.com>
2022-02-09 08:04:10 -0300

    recomeçando script e baixando bases do curso

---
+++ R/S_principal.R
@@ -0,0 +1,7 @@
+# bom, acabou o espaço no meu computador C, tive que ir para o D,
+# e por alguma razão perdi todo o conteúdo do meu script.
+
+
+# Exercicios aula 2 - tidyr -----------------------------------------------
+
+
